b1fcfc3a48aa13bbbf4508bf2e9dc8c79c0342ba
harsh <harsh@harshs-MacBook-Air.local>
2025-02-05 10:43:36 +0530

    link button

--- src/Side branches/Exam.jsx
+++ src/Side branches/Exam.jsx
@@ -10,17 +10,17 @@ const Exam = () => {
             <div className='exam_box1'>
                 <h2>Engineering </h2>
                 <p>Preparing for  JEE(main), JEE(advanced), GATE, etc.</p>
-                <button className='exam-btn'><Link to='/start_page'>Get Started</Link></button>
+                <Link to='/start_page'><button className='exam-btn'>Get Started</button></Link>
             </div>
             <div className='exam_box2'> 
                 <h2>Medical</h2>
                 <p>Preparing for NEET UG ,NEET PG , AIPMT, etc.</p>
-                <button className='exam-btn'><Link to='/start_page'>Get Started</Link></button>
+                <Link to='/start_page'><button className='exam-btn'>Get Started</button></Link>
             </div>
             <div className='exam_box3'>
                 <h2>Other Exams</h2>
                 <p>Prepare for CLAT, BBA, BCA,CBSE Board Exams, etc.</p>
-                <button className='exam-btn'><Link to='/start_page'>Get Started</Link></button>
+                <Link to='/start_page'><button className='exam-btn'>Get Started</button></Link>
             </div>
         </div>
       </div>
